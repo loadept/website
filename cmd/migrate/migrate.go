e6043b7d03d9f877d528cf4384099b558d84675d
loadept <loadept3@gmail.com>
2026-02-19 22:14:16 -0500

    refactor: streamline migration process by removing unused config and file handling

--- cmd/migrate/migrate.go
+++ cmd/migrate/migrate.go
@@ -3,59 +3,53 @@ package main
 import (
 	"flag"
 	"log"
-	"os"
-	"path/filepath"
-	"sort"
 
-	"github.com/loadept/loadept.com/internal/config"
 	"zombiezen.com/go/sqlite"
 	"zombiezen.com/go/sqlite/sqlitex"
 )
 
+const schema = `
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    identifier TEXT NOT NULL UNIQUE,
+    github_id INTEGER NOT NULL UNIQUE,
+    github_login TEXT NOT NULL,
+    github_node_id TEXT NOT NULL,
+    status TEXT NOT NULL CHECK(status IN ('active', 'deleted')) DEFAULT 'active',
+    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TEXT 
+) STRICT;
+CREATE INDEX IF NOT EXISTS idx_users_identifier ON users(identifier);
+
+CREATE TABLE IF NOT EXISTS short_urls (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    original_url TEXT NOT NULL,
+    short_code TEXT NOT NULL UNIQUE,
+    status TEXT NOT NULL CHECK(status IN ('active', 'deleted')) DEFAULT 'active',
+    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TEXT
+) STRICT;
+CREATE INDEX IF NOT EXISTS idx_short_code ON short_urls(short_code);
+CREATE INDEX IF NOT EXISTS idx_original_url ON short_urls(original_url);
+`
+
 func main() {
 	log.SetFlags(0)
 
-	var confFile string
-	flag.StringVar(&confFile, "config", "", "config file for server")
+	dbPath := flag.String("db", "db.sqlite3", "Path to the SQLite database")
 	flag.Parse()
 
-	if confFile == "" {
-		log.Fatal("config file not specified")
-	}
-
-	cfg, err := config.Load(confFile)
-	if err != nil {
-		log.Fatalf("internal error: %v", err)
-	}
-
-	conn, err := sqlite.OpenConn(cfg.Database.DBPath, sqlite.OpenReadWrite, sqlite.OpenCreate)
+	conn, err := sqlite.OpenConn(*dbPath, sqlite.OpenReadWrite, sqlite.OpenCreate)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
-	dirEntry, err := os.ReadDir(cfg.Database.MigrationsPath)
-	if err != nil {
+	if err := sqlitex.ExecuteScript(conn, schema, nil); err != nil {
 		log.Fatal(err)
-	}
-	sort.Slice(dirEntry, func(i, j int) bool {
-		return dirEntry[i].Name() < dirEntry[j].Name()
-	})
-
-	for _, entry := range dirEntry {
-		if entry.IsDir() || filepath.Ext(entry.Name()) != ".sql" {
-			continue
-		}
-
-		migration := filepath.Join(cfg.Database.MigrationsPath, entry.Name())
-		content, err := os.ReadFile(migration)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := sqlitex.ExecuteScript(conn, string(content), nil); err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("applied migration: %s", entry.Name())
 	}
 	log.Println("all migrations applied")
 }
