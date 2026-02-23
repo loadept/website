3128ea9a51abc0051dd130ae657c6847f5f2d6bc
loadept <loadept3@gmail.com>
2026-02-22 23:10:29 -0500

    remove unused ShortURL methods and clean up error handling

--- internal/storage/shorturl.go
+++ internal/storage/shorturl.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"strings"
 
-	"zombiezen.com/go/sqlite"
 	"zombiezen.com/go/sqlite/sqlitex"
 )
 
@@ -21,25 +19,12 @@ func NewShortURLStorage(db *sqlitex.Pool) (*ShortURLStorage, error) {
 	return &ShortURLStorage{db: db}, nil
 }
 
-var (
-	ErrShortURLNotFound         = errors.New("short url does not exist or invalid short code")
-	ErrShortURLInsertNotRow     = errors.New("insert short url returned no rows")
-	ErrShortURLCodeExists       = errors.New("the provided shortcode already exists")
-	ErrShortURLNameExists       = errors.New("the provided name already exists")
-	ErrShortURLConstraintUnique = errors.New("a unique constraint was violated")
-)
+var ErrShortURLNotFound = errors.New("short url does not exist or invalid short code")
 
 const (
 	URLStatusActive  = "active"
 	URLStatusDeleted = "deleted"
 )
-
-type ShortURL struct {
-	ID          int64  `json:"id,omitempty"`
-	Name        string `json:"name"`
-	OriginalURL string `json:"original_url"`
-	ShortCode   string `json:"short_code,omitempty"`
-}
 
 func (s *ShortURLStorage) GetURL(ctx context.Context, shortCode string) (string, error) {
 	conn, err := s.db.Take(ctx)
@@ -68,49 +53,4 @@ func (s *ShortURLStorage) GetURL(ctx context.Context, shortCode string) (string,
 		return "", ErrShortURLNotFound
 	}
 	return stmt.GetText("original_url"), nil
-}
-
-func (s *ShortURLStorage) SaveURL(ctx context.Context, data *ShortURL) (*ShortURL, error) {
-	conn, err := s.db.Take(ctx)
-	if err != nil {
-		return nil, err
-	}
-	defer s.db.Put(conn)
-
-	stmt, err := conn.Prepare(`
-		INSERT INTO short_urls(name, original_url, short_code)
-		VALUES ($1, $2, $3)
-		RETURNING id, name, original_url, short_code
-	`)
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Reset()
-
-	stmt.SetText("$1", data.Name)
-	stmt.SetText("$2", data.OriginalURL)
-	stmt.SetText("$3", data.ShortCode)
-
-	if hasRow, err := stmt.Step(); err != nil {
-		if sqlite.ErrCode(err) == sqlite.ResultConstraintUnique {
-			if strings.Contains(err.Error(), "short_urls.short_code") {
-				return nil, ErrShortURLCodeExists
-			}
-			if strings.Contains(err.Error(), "short_urls.name") {
-				return nil, ErrShortURLNameExists
-			}
-			return nil, ErrShortURLConstraintUnique
-		}
-		return nil, err
-	} else if !hasRow {
-		return nil, ErrShortURLInsertNotRow
-	}
-
-	insertedData := &ShortURL{
-		ID:          stmt.GetInt64("id"),
-		Name:        stmt.GetText("name"),
-		OriginalURL: stmt.GetText("original_url"),
-		ShortCode:   stmt.GetText("short_code"),
-	}
-	return insertedData, nil
 }
